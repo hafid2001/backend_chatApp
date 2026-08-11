4a748c1deb59142f3ca35cc687d5144b5114a993
matela <abdou26072001@gmail.com>
2026-08-11 11:00:14 +0100

    creat user model

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import http from "http";
+import { connectDB } from "./lib/db.js";
 
 
 //Creat Express app and http server
@@ -16,6 +17,8 @@ app.use(cors());
 
 app.use("/api/status",(req,res)=> res.send("server is live"));
 
+//connection to db 
+await connectDB();
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT,()=> console.log("Server is running on PORT: "+ PORT));
