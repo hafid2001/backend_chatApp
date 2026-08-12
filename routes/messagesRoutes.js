052228385b97ad4a11aea5c0774de8b5763d736e
matela <abdou26072001@gmail.com>
2026-08-12 11:25:48 +0100

    finich the backend

--- routes/messagesRoutes.js
+++ routes/messagesRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getMessages, getUserForSidebar, markMessageAsseen } from "../controller/messageController.js";
+import { getMessages, getUserForSidebar, markMessageAsseen, sendmessage } from "../controller/messageController.js";
 import {protectRoute} from "../Middlware/auth.js"
 
 
@@ -9,5 +9,7 @@ const messageRouter = express.Router();
 messageRouter.get("/users",protectRoute,getUserForSidebar);
 messageRouter.get("/:id",protectRoute,getMessages);
 messageRouter.put("/mark/:id",protectRoute,markMessageAsseen);
+messageRouter.post("/send/:id",protectRoute,sendmessage);
+
 
 export default messageRouter;
